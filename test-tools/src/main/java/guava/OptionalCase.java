b13f03dcbd42205da0c6a52f1d1c9e4393dd1ec1
wdx <wangdongxun1010@qq.com>
2018-01-16 17:13:56 +0800

    option modify

--- test-tools/src/main/java/guava/OptionalCase.java
+++ test-tools/src/main/java/guava/OptionalCase.java
@@ -5,6 +5,8 @@ import com.google.common.base.Optional;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Optional.ofNullable;
+
 /**
  * Created by wdx on 2016/11/10.
  */
@@ -14,7 +16,11 @@ public class OptionalCase {
         m.put("one",true);
         m.put("two",false);
 
-        Boolean aBoolean=Optional.of(m.get("three")).orNull();
-        System.out.println(aBoolean);
+        //jdk 1.8  实现
+        System.out.println(ofNullable(m.get("one")).isPresent());
+        System.out.println(ofNullable(m.get("three")).isPresent());
+        //google guava 实现
+        System.out.println(Optional.fromNullable(m.get("one")).isPresent());
+        System.out.println(Optional.fromNullable(m.get("three")).isPresent());
     }
 }
